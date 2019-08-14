d3da5c098187548145321234044b9bcd37588c6c
Vitor Demenighi <vitor_m2@hotmail.com>
2019-08-14 16:55:50 -0300

    add get now playing movies request example

--- ArchitectureChallenge/View/ViewController.swift
+++ ArchitectureChallenge/View/ViewController.swift
@@ -12,8 +12,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.request()
+        
     }
 
+    func request(){
+        if let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=79bb37b9869aa0ed97dc7a23c93d0829&language=en-US&page=1"){
+            let task = URLSession.shared.dataTask(with: url) { (data, request, err) in
+                if err == nil {
+                    if let dadosRetorno = data {
+                        let request = try! JSONDecoder().decode(Request.self, from: dadosRetorno)
+                
+                        print(request.results[1])
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
 
 }
 
