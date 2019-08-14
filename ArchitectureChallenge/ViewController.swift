3d035266ef62bbae63872fe7bbba71d9ee5e27be
Vitor Demenighi <vitor_m2@hotmail.com>
2019-08-14 15:18:49 -0300

    first commit

---
+++ ArchitectureChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArchitectureChallenge
+//
+//  Created by Vitor Demenighi on 14/08/19.
+//  Copyright © 2019 Vitor Demenighi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
