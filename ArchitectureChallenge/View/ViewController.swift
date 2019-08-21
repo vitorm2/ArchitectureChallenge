e1d7b133e1bfbb9fbbbc45c40f35045255b7e8fb
Rovane Moura <mourarovane@gmail.com>
2019-08-21 13:52:27 -0300

    Navigation Controller is Search Results Controller

--- ArchitectureChallenge/View/ViewController.swift
+++ ArchitectureChallenge/View/ViewController.swift
@@ -79,7 +79,7 @@ class ViewController: UIViewController, ListViewDelegate, HeaderDelegate {
     func setupNavBar() {
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        let searchController = UISearchController(searchResultsController: resultsViewController)
+        let searchController = UISearchController(searchResultsController: resultsNavigationController)
         searchController.searchResultsUpdater = resultsViewController
         searchController.obscuresBackgroundDuringPresentation = true
         searchController.delegate = resultsViewController
